0a022805518ce07ab427116a88dff22ba6913b7e
MMMayongtian <298985343@qq.com>
2023-02-13 22:22:02 +0800

    Modify the home page layout style

--- frontend/views/team/index.php
+++ frontend/views/team/index.php
@@ -1,4 +1,17 @@
+<?php
+
+use frontend\widgets\TeamListView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\TeamSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Teams';
+$this->params['breadcrumbs'][] = yii::t('app', 'Team');
+?>
 
 <div>
-
+    <?= TeamListView::widget([
+        'dataProvider' => $dataProvider,
+    ]) ?>
 </div>
